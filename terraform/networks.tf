eefa1afc2baaa8bf769e02d5d604187a731cc6ee
Rayhan Kimi <r4yhankimi@gmail.com>
2026-07-14 11:32:58 +0800

    init

---
+++ terraform/networks.tf
@@ -0,0 +1,133 @@
+resource "aws_vpc" "main" {
+    cidr_block = "10.0.0.0/16"
+    enable_dns_hostnames = true
+    enable_dns_support = true
+
+    tags= {
+        Name = "${local.env}-main"
+    }
+}
+
+########### IGW ########### 
+resource "aws_internet_gateway" "igw" {
+    vpc_id = aws_vpc.main.id
+    tags = {
+        Name = "${local.env}-main"
+    }
+}
+########### IGW ########### 
+
+########### SUBNET ########### 
+resource "aws_subnet" "private_zone1" {
+    vpc_id = aws_vpc.main.id
+    cidr_block = "10.0.0.0/19"
+    availability_zone = local.zone1
+
+    tags = {
+        "Name"  = "${local.env}-private-${local.zone1}",
+        "kubernetes.io/role/internal-elb" = "1",
+        "kubernetes.io/cluster/${local.env}-${local.eks_name}" = "owned"
+    }
+}
+
+resource "aws_subnet" "private_zone2" {
+    vpc_id = aws_vpc.main.id
+    cidr_block = "10.0.32.0/19"
+    availability_zone = local.zone2
+
+    tags = {
+        "Name"  = "${local.env}-private-${local.zone1}",
+        "kubernetes.io/role/internal-elb" = "1",
+        "kubernetes.io/cluster/${local.env}-${local.eks_name}" = "owned"
+    }
+}
+
+resource "aws_subnet" "public_zone1" {
+    vpc_id = aws_vpc.main.id
+    cidr_block = "10.0.64.0/19"
+    availability_zone = local.zone1
+    map_public_ip_on_launch = true
+
+    tags = {
+        "Name"  = "${local.env}-private-${local.zone1}",
+        "kubernetes.io/role/internal-elb" = "1",
+        "kubernetes.io/cluster/${local.env}-${local.eks_name}" = "owned"
+    }
+}
+
+resource "aws_subnet" "public_zone2" {
+    vpc_id = aws_vpc.main.id
+    cidr_block = "10.0.96.0/19"
+    availability_zone = local.zone2
+    map_public_ip_on_launch = true
+
+    tags = {
+        "Name"  = "${local.env}-private-${local.zone1}",
+        "kubernetes.io/role/internal-elb" = "1",
+        "kubernetes.io/cluster/${local.env}-${local.eks_name}" = "owned"
+    }
+}
+########### SUBNET ########### 
+
+########### NAT ########### 
+resource "aws_eip" "nat" {
+    domain = "vpc"
+    tags = {
+        Name = "${local.env}-nat"
+    }
+}
+
+resource "aws_nat_gateway" "nat" {
+    allocation_id = aws_eip.nat.id
+    subnet_id = aws_subnet.public_zone1
+    depends_on = [ aws_internet_gateway.igw ]
+}
+########### NAT ########### 
+
+########### RT ########### 
+resource "aws_route_table" "private" {
+    vpc_id = aws_vpc.main.id
+
+    route = {
+        cidr_block = "0.0.0.0/0"
+        nat_gateway_id = aws_nat_gateway.nat.id
+    }
+
+    tags = {
+        Name = "${local.env}-private"
+    }
+}
+
+resource "aws_route_table" "public" {
+    vpc_id = aws_vpc.main.id
+
+    route = {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = aws_internet_gateway.igw.id
+    }
+
+    tags = {
+        Name = "${local.env}-public"
+    }
+}
+
+resource "aws_route_table_association" "private_zone1" {
+    route_table_id = aws_route_table.private
+    subnet_id = aws_subnet.private_zone1
+}
+
+resource "aws_route_table_association" "private_zone2" {
+    route_table_id = aws_route_table.private
+    subnet_id = aws_subnet.private_zone2
+}
+
+resource "aws_route_table_association" "public_zone1" {
+    route_table_id = aws_route_table.public
+    subnet_id = aws_subnet.public_zone1
+}
+
+resource "aws_route_table_association" "public_zone2" {
+    route_table_id = aws_route_table.public
+    subnet_id = aws_subnet.public_zone2
+}
+########### RT ########### 
