7b7431f57415d8f9a3c163f05c53f8f0abbc79b0
Rayhan Kimi <r4yhankimi@gmail.com>
2026-07-14 11:36:41 +0800

    fix lint

--- terraform/locals.tf
+++ terraform/locals.tf
@@ -1,8 +1,8 @@
 locals {
-    env         = "staging"
-    region      = "ap-southeast-1"
-    zone1       = "ap-southeast-1a"
-    zone2       = "ap-southeast-1b"
-    eks_name    = "demo-cluster"
-    eks_version     = "1.36"
+  env         = "staging"
+  region      = "ap-southeast-1"
+  zone1       = "ap-southeast-1a"
+  zone2       = "ap-southeast-1b"
+  eks_name    = "demo-cluster"
+  eks_version = "1.36"
 }
